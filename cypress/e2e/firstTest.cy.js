bb74c1740be422b05443f73fdcb4e3c73161b2d1
Andrei Gaponenko <puhpuh690@gmail.com>
2022-08-30 09:15:22 +0300

    Adds tests with contains

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,2 +1,21 @@
 /// <reference types="Cypress"/>
 
+it('', () => {
+    cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
+    cy.contains('Search')
+});
+
+it('', () => {
+    cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
+    cy.contains('span', 'Search')
+});
+
+it('', () => {
+    cy.visit('https://docs.cypress.io/api/commands/eq#Syntax')
+    cy.contains('SEARCH', {matchCase: false})
+});
+
+it.only('', () => {
+    cy.visit('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter')
+    cy.get('li').contains('Get MDN Plus')
+});
